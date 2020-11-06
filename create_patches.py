23ee758730b8c20b22154a4efeb23a6815b3d212
LisaBa <lisa.bareiss@posteo.de>
2020-11-06 15:28:28 +0100

    Edit file

--- create_patches.py
+++ create_patches.py
@@ -1,6 +1,8 @@
 import numpy as np
-import dask
+import dask.array as da
 
 # Read file as dask array
+filename = r'C:\Users\Lisapisa\Documents\Master\Masterthesis\original_data\Landsberg\BaseImage.tif'
+print("Filename: ", filename)
 
 # Create patches and write into new files
