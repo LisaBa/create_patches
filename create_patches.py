065d40da1cd8c9a9c390df94e6f2d6a2f31d3490
LisaBa <lisa.bareiss@posteo.de>
2020-12-17 16:10:37 +0100

    Edit

--- create_patches.py
+++ create_patches.py
@@ -2,13 +2,24 @@ import numpy as np
 import dask.array as da
 import xarray as xr
 import rasterio
+from rasterio.windows import Window
 import rioxarray as riox
 import matplotlib.pyplot as plt
+import os
 
 
 class PatchCreator:
-    def __init__(self, filename, n_bands=3, chunks_x=256, chunks_y=256):
+    def __init__(
+        self,
+        filename,
+        output_folder,
+        index_start=0,
+        n_bands=3,
+        chunks_x=256,
+        chunks_y=256,
+    ):
         self.filename = filename
+        self.output_folder = output_folder
         self.n_bands = n_bands
         self.chunks_x = chunks_x
         self.chunks_y = chunks_y
@@ -35,22 +46,53 @@ class PatchCreator:
         return data
 
     def create_subset(self):
-        subset = self.data[:, 20000:22000, 10000:12000]
-        print(subset)
-        return subset
+        # subset = self.data[:, 20000:22000, 10000:12000]
+        # print(subset)
+        # return subset
+        win = Window(0, 0, 256, 256)
+        with rasterio.open(self.filename) as src:
+            w = src.read(window=win)
+        print(w.shape)
+
+        # write meta data
+        xform = rasterio.windows.transform(win, src.meta["transform"])
+        meta_d = src.meta.copy()
+        meta_d.update(
+            {"driver": "GTiff", "height": 256, "width": 256, "transform": xform}
+        )
+        # write output
+        with rasterio.open(
+            os.path.join(self.output_folder, "images", "patch.tif"), "w", **meta_d
+        ) as dest:
+            dest.write(w)
 
     def plot_data(self, x):
         x = x.transpose("x", "y", "band")
         plt.imshow(x[:, :, :])
         plt.show()
 
-    def create_patches(self):
+    def create_patches(self, patch_size=256):
+        # Create output folders for images and labels
+        # os.mkdir(os.path.join(self.output_folder, "images"))
+        # os.mkdir(os.path.join(self.output_folder, "labels"))
+
         # If its not dividable it need padding!
-        # Do I even need to export again, or can I just work with the xarray object?
-        patch = self.data[:, 20000:20256, 10000:10256]
-        # patch.rio.to_raster(
-        #     r"C:\Users\Lisapisa\Documents\Master\Masterthesis\test_patch.tif"
-        # )
+        # I need to export the patches because I need to upload them to the google drive
+        patch = self.data[:, :256, :256]
+        print(patch)
+        self.plot_data(patch)
+        with rasterio.open(
+            os.path.join(self.output_folder, "images", "patch.tif"),
+            "w",
+            driver="GTiff",
+            height=256,
+            width=256,
+            count=3,
+            dtype=patch.dtype,
+            crs="+init=epsg:32632",
+        ) as dst:
+            dst.write(patch)
+        # patch.rio.to_raster(os.path.join(self.output_folder, "images", "patch.tif"))
         print("Patch created: ", patch)
         return patch
 
--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 from create_patches import PatchCreator
+import xarray as xr
+import rasterio
 
 # Read file as xarray in chunks of 3x256x256
-filename = r"C:\Users\Lisapisa\Documents\Master\Masterthesis\original_data\Landsberg\BaseImage.tif"
+filename = (
+    "C:/Users/Lisapisa/Documents/Master/Masterthesis/01-Raster/annotated_area.tif"
+)
+output_folder = "C:/Users/Lisapisa/Documents/Master/Masterthesis/01-Raster/samples_auto"
 
-new = PatchCreator(filename)
+new = PatchCreator(filename, output_folder)
 new.print_info()
+new.create_subset()
 
 # Create patches and write into new files
 # new.create_patches()
-sub = new.create_subset()
-new.plot_data(x=sub)
-patch = new.create_patches()
-new.plot_data(patch)
+# sub = new.create_subset()
+# new.plot_data(x=sub)
+# new.create_patches()
+
+# test = "C:/Users/Lisapisa/Documents/Master/Masterthesis/01-Raster/samples/images/000000000.tif"
+# t = xr.open_rasterio(test)
+# print(t)
